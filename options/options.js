f60c51f7a96c1e9bc66d60d6fa3240777e28ba1c
Crowdbotics <engineering@crowdbotics.com>
2023-03-13 18:31:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "modelbilder-dev-69221",
+  url: "https://modelbilder-dev-69221.botics.co",
+  api: "https://modelbilder-dev-69221.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
